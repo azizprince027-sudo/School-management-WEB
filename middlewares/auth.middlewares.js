ec53525cbfc9f62cf13748aebc915b32667ef1e2
Odjo Aziz <azizprince027@gmail.com>
2026-07-30 17:04:02 +0000

    modification de auh midelware

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,24 +1,18 @@
-const jwt = require('jsonwebtoken');
-
-// Verifie la presence et la validite du token JWT dans le header Authorization
-// Format attendu : "Authorization: Bearer <token>"
-function authMiddleware(req, res, next) {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Token manquant.' });
-    }
-
-    const token = authHeader.split(' ')[1];
-
+const express = require('express');
+const server  =  require("../server.js")
+const authControlleur = require('../controller/auth.controller');
+const loginUser = require('../services/authetification.js');
+const loginStudent = require('../services/authetification.js');
+async function authMiddleware(req, res, next) {
     try {
-        // decode le token et verifie sa signature grace a la cle secrete du .env
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // { id, role, name/matricule } accessible dans tous les controllers suivants
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: 'Token invalide ou expire.' });
+        if (authControlleur.connexion !== loginUser ) {
+            res.status (401)
+        }
+    } catch (error) {
+        
     }
+
+
 }
 
 module.exports = {authMiddleware};
